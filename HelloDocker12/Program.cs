a0f8bebdc3f1de2937cde197f9310dcd44ed1c7e
Novikova Anastasia <novikova29292828@gmail.com>
2024-12-22 17:31:31 +0300

    Добавьте файлы проекта.

---
+++ HelloDocker12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Dоcker!");
+        }
+    }
+}
